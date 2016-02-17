653eab0e885719caf9385ecca45871b8c4b13ed7
Rafael Mallare <rafael.mallare@ttu.edu>
2016-02-16 23:06:01 -0600

    Testing TableRow visibility

--- app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
+++ app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
@@ -2,6 +2,9 @@ package com.softwareengineering.ttu.shadowruncharactermanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class CharacterSheet extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class CharacterSheet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_sheet);
+/*
+        TableRow r = (TableRow) findViewById(R.id.table_row_1);
+        r.setVisibility(View.GONE);
+        */
     }
+
 }
