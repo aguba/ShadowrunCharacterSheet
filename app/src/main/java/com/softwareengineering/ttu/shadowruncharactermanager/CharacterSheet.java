eb5a2b5b3b41e4dd9ad0995a8aa30755907a859b
Rafael Mallare <rafael.mallare@ttu.edu>
2016-02-19 23:33:31 -0600

    Filled in list of skills in Skills card. Added dicepool and damage readings for Equipped Weapon card. Adjusted layout of Character card. Added health and stun tracker bars to character card.

--- app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
+++ app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
@@ -3,8 +3,10 @@ package com.softwareengineering.ttu.shadowruncharactermanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class CharacterSheet extends AppCompatActivity {
 
@@ -16,6 +18,7 @@ public class CharacterSheet extends AppCompatActivity {
         TableRow r = (TableRow) findViewById(R.id.table_row_1);
         r.setVisibility(View.GONE);
         */
+
     }
 
 }
