f8f63975c307bfa467a4d6789062a431198a2cd7
Rafael Mallare <rafael.mallare@ttu.edu>
2016-02-27 01:25:18 -0600

    Added blank activities for expanded character and skills cards and tabbed activity for expanded equipment card. Adjusted layout of health bars. Adjusted margins of cards.

---
+++ app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterCard.java
@@ -0,0 +1,14 @@
+package com.softwareengineering.ttu.shadowruncharactermanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_card);
+    }
+
+}
--- app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
+++ app/src/main/java/com/softwareengineering/ttu/shadowruncharactermanager/CharacterSheet.java
@@ -1,13 +1,11 @@
 package com.softwareengineering.ttu.shadowruncharactermanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
-import java.util.ArrayList;
 
 public class CharacterSheet extends AppCompatActivity {
 
@@ -22,6 +20,9 @@ public class CharacterSheet extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_sheet);
         initialize();
+
+        pHealth.setText(String.valueOf(pHealthBar.getMax()) + "/" + String.valueOf(pHealthBar.getMax()));
+        sHealth.setText(String.valueOf(sHealthBar.getMax()) + "/" + String.valueOf(sHealthBar.getMax()));
 /*
         TableRow r = (TableRow) findViewById(R.id.table_row_1);
         r.setVisibility(View.GONE);
@@ -30,12 +31,14 @@ public class CharacterSheet extends AppCompatActivity {
         pHealthBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                pHealth.setText(String.valueOf(progress));
+                pHealth.setText(String.valueOf(progress) + "/" + String.valueOf(pHealthBar.getMax()));
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
@@ -45,12 +48,14 @@ public class CharacterSheet extends AppCompatActivity {
         sHealthBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                sHealth.setText(String.valueOf(progress));
+                sHealth.setText(String.valueOf(progress)+ "/" + String.valueOf(sHealthBar.getMax()));
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
@@ -59,13 +64,27 @@ public class CharacterSheet extends AppCompatActivity {
 
     }
 
-    void initialize(){
+    void initialize() {
         pHealth = (TextView) findViewById(R.id.physical_health_value);
         pHealthBar = (SeekBar) findViewById(R.id.physical_health_bar);
         sHealth = (TextView) findViewById(R.id.stun_health_value);
         sHealthBar = (SeekBar) findViewById(R.id.stun_health_bar);
     }
 
+    public void expandCharacter(View view){
+        Intent characterCard = new Intent(CharacterSheet.this, CharacterCard.class);
+        CharacterSheet.this.startActivity(characterCard);
+    }
+
+    public void expandSkills(View view){
+        Intent skillsCard = new Intent(CharacterSheet.this, SkillsCardActivity.class);
+        CharacterSheet.this.startActivity(skillsCard);
+    }
+
+    public void expandEquipment(View view){
+        Intent equipmentCard = new Intent(CharacterSheet.this, EquipmentActivity.class);
+        CharacterSheet.this.startActivity(equipmentCard);
+    }
 
 
 }
